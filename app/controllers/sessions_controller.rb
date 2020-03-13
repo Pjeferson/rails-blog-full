0cd5d95cf5c68c7b74c3c00c4efa30680ddbb515
Pjeferson <paulojeferson34@gmail.com>
2020-03-13 17:19:37 -0300

    Criando controller para gerenciar sessões do usuário

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,22 @@
+class SessionsController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.find_by_email(params[:email])
+    if @user && @user.authenticate(params[:password])
+      session[:user_id] = @user.id
+      redirect_to root_url, notice: 'Sucesso em realizar login!'
+    else
+      flash.now[:alert] = 'Email ou senha inválidos, verifique seus dados.'
+      render 'new'
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    redirect_to root_url, notice: 'Sucesso em realizar logout!'
+
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
+  get 'sessions/destroy'
   root 'pages#index'
 
   get '/signup', to: 'users#new'
+  get '/login', to: 'sessions#new'
+  get '/logout', to: 'sessions#destroy'
 
   resources :users
+  resources :sessions, only: [:new, :create, :destroy]
 end
