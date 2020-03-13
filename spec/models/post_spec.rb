d8b4fdd2201df9307e87898022c796f6768f84a1
Pjeferson <paulojeferson34@gmail.com>
2020-03-13 13:39:13 -0300

    Refatorando inserção de Usuário na criação de Post

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -9,27 +9,27 @@ RSpec.describe Post, type: :model do
   
   # Validation
   it 'should be valid with valide attributes' do
-    post = Post.new(user_id: user.id, title: 'Post Title', body: 'body text')
+    post = Post.new(user: user, title: 'Post Title', body: 'body text')
     expect(post).to be_valid  
   end
 
   it 'should be valid without body' do
-    post = Post.new(user_id: user.id, title: 'Post Title', body: '')
+    post = Post.new(user: user, title: 'Post Title', body: '')
     expect(post).to be_valid  
   end
 
   it 'should not be valid without title' do
-    post = Post.new(user_id: user.id, title: '', body: 'body text')
+    post = Post.new(user: user, title: '', body: 'body text')
     expect(post).not_to be_valid  
   end
 
   it 'should not be valid with a title bigger than 50 chars' do
-    post = Post.new(user_id: user.id, title: 'a' * 51, body: 'body text')
+    post = Post.new(user: user, title: 'a' * 51, body: 'body text')
     expect(post).not_to be_valid  
   end
 
   it 'should not be valid with a body bigger than 240 chars' do
-    post = Post.new(user_id: user.id, title: 'a' * 241, body: 'body text')
+    post = Post.new(user: user, title: 'a' * 241, body: 'body text')
     expect(post).not_to be_valid  
   end
 end
