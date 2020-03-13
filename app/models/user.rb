928c78c44dce94be395b788a3f43c517b7a32912
Pjeferson <paulojeferson34@gmail.com>
2020-03-13 13:38:15 -0300

    Adicionado relação de seguidores no modelo de Usuário

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,15 @@
 class User < ApplicationRecord
     has_many :posts
 
+    has_many :follows
+   
+    has_many :followers_relations, foreign_key: :following_id, class_name: 'Follow'
+    has_many :followers, through: :followers_relations, source: :follower
+
+    has_many :following_relations, foreign_key: :follower_id, class_name: 'Follow'
+    has_many :following, through: :following_relations, source: :following
+
+
     validates :name, presence: true, length: {maximum: 50}
     validates :email, presence: true, length: {maximum: 255}
     has_secure_password
