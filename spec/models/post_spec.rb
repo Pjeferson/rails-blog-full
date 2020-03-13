3fbbe170cd1df7e2bb5f414d9ba9b5b7b457dafd
Pjeferson <paulojeferson34@gmail.com>
2020-03-13 10:34:07 -0300

    Adicionando casos de teste para o módulo Post

---
+++ spec/models/post_spec.rb
@@ -0,0 +1,30 @@
+require 'rails_helper'
+
+RSpec.describe Post, type: :model do
+  # Validation
+
+  it 'should be valid with valide attributes' do
+    post = Post.new(title: 'Post Title', body: 'body text')
+    expect(post).to be_valid  
+  end
+
+  it 'should be valid without body' do
+    post = Post.new(title: 'Post Title', body: '')
+    expect(post).to be_valid  
+  end
+
+  it 'should not be valid without title' do
+    post = Post.new(title: '', body: 'body text')
+    expect(post).not_to be_valid  
+  end
+
+  it 'should not be valid with a title bigger than 50 chars' do
+    post = Post.new(title: 'a' * 51, body: 'body text')
+    expect(post).not_to be_valid  
+  end
+
+  it 'should not be valid with a body bigger than 240 chars' do
+    post = Post.new(title: 'a' * 241, body: 'body text')
+    expect(post).not_to be_valid  
+  end
+end
