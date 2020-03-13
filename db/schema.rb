7a75d9408db0760da8ed2b6bac2c1d27b19d7394
Pjeferson <paulojeferson34@gmail.com>
2020-03-13 13:16:11 -0300

    Criando relação de Usuários como seguidores

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_13_132231) do
+ActiveRecord::Schema.define(version: 2020_03_13_143521) do
+
+  create_table "follows", force: :cascade do |t|
+    t.integer "follower_id"
+    t.integer "following_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "posts", force: :cascade do |t|
     t.integer "user_id"
