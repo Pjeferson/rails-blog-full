d384afbdbc5169d2ca0130373fa3eb6826f9b2cc
Pjeferson <paulojeferson34@gmail.com>
2020-03-13 18:04:35 -0300

    Criando rota para listar usuários não seguidos e utilizando-a

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :destroy]
 
   def index
-    @users = User.all
+    @users = User.joins("left join follows on follower_id != #{current_user.id}").where("users.id != #{current_user.id}")
   end
 
   def show
