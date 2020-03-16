71c5f891b6cc6e60679f607f90f5faa31d927d4b
Pjeferson <paulojeferson34@gmail.com>
2020-03-16 01:52:28 -0300

    Filtrando posts por usuários seguidos

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,7 +2,7 @@ class PostsController < ApplicationController
   before_action :set_post, only: [:show, :edit, :update, :destroy]
 
   def index
-    @posts = Post.joins(:user).all.order('created_at DESC')
+    @posts = Post.where(:user_id => current_user.following).order('created_at DESC')
   end
 
   def show
