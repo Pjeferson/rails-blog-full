4ff053d05b3a585701bbded2e8761c220991471a
Pjeferson <paulojeferson34@gmail.com>
2020-03-16 09:53:21 -0300

    Adicionando um teste para a tentativar de seguir usuário inexistentes

--- spec/models/follow_spec.rb
+++ spec/models/follow_spec.rb
@@ -5,18 +5,23 @@ RSpec.describe Follow, type: :model do
   let(:user2) { User.create(name: 'My Name2', email: 'email2@email.com', password: 'mypassword', password_confirmation: 'mypassword')}
 
 
-   # Associations
-   it { should belong_to(:follower) }
-   it { should belong_to(:following) }
+  # Associations
+  it { should belong_to(:follower) }
+  it { should belong_to(:following) }
   
-   # Validation
-   it 'should be valid with valide attributes' do
-     follow = Follow.new(follower: user1, following: user2)
-     expect(follow).to be_valid  
-   end
+  # Validation
+  it 'should be valid with valide attributes' do
+    follow = Follow.new(follower: user1, following: user2)
+    expect(follow).to be_valid  
+  end
 
-   it 'should not be valid without users' do
+  it 'should not be valid without users' do
     follow = Follow.new(follower: user1)
+    expect(follow).to_not be_valid  
+  end
+
+  it 'should not be valid with not existent users' do
+    follow = Follow.new(follower_id: 3, following_id: 4)
     expect(follow).to_not be_valid  
   end
 end
